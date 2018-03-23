847965684c68bf29f1110545a92bb2fa0f616c19
Nico Blasi <domenico.blasi.acc@mef.gov.it>
2018-03-23 13:19:55 +0100

    primo commit

---
+++ app.js
@@ -0,0 +1,25 @@
+var http = require('http');
+var server = new http.Server();
+var port = 8000;
+
+/*
+server.on('request', (request, response) => {
+    console.log('=========== REQUEST ===========');
+    console.log('Url: ', request.url);
+    console.log('Method: ', request.method);
+    console.log('User-agent: ', request.headers['user-agent']);
+    response.writeHead(200, {
+        'Content-type': 'text/plain'
+    });
+    response.end('Ciao Nico, sei troppo forte!');
+});
+server.listen(port);
+*/
+
+
+http.createServer((req, res) => {
+    res.writeHead(200, {
+        'Content-type': 'text/plain'
+    });
+    res.end('Ciao Nico, sei troppo giusto!');
+}).listen(port);
